4d412d2f016c454923472496a854d6fcdd56015c
knjain <knjain1999@gmail.com>
2023-12-14 12:15:28 +0530

    login page changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import "./App.css";
-import { Login } from "./components/login";
 import { ForgotPassword } from "./components/ForgotPassword";
 import { Route, Routes } from "react-router-dom";
 import { Signup } from "./components/Signup";
-import { Upichecker} from "./components/upichecker";
-
+import { Loginn } from "./components/Loginn";
 
 function App() {
   return (
     <>
      
       <Routes>
-        <Route index element={<Login />} />
+        <Route index element={<Loginn />} />
         <Route path="/forgotpassword" element={<ForgotPassword />} />
         <Route path="/signup" element={<Signup />} />
         {/* <Route path="/........" element={<Upichecker />} /> */}
